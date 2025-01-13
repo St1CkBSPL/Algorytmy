120db2740eed7267f32e2e49320c42634f36f205
St1CkBSPL <147150184+St1CkBSPL@users.noreply.github.com>
2025-01-13 12:54:37 +0100

    Add files via upload

---
+++ NajdluszyPodciag/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace NajdluszyPodciag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(413, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(431, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(357, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(296, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GenerujNWP\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(296, 202);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ NajdluszyPodciag/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NajdluszyPodciag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sequence1 = textBox1.Text;
+            string sequence2 = textBox2.Text;
+
+            string nwpResult = FindNwp(sequence1, sequence2);
+            textBox3.Text = "NWP: " + nwpResult;
+        }
+
+        private string FindNwp(string sequence1, string sequence2)
+        {
+            int dlugosc1 = sequence1.Length;
+            int dlugosc2 = sequence2.Length;
+            int[,] dot = new int[dlugosc1 + 1, dlugosc2 + 1];
+
+            for (int i = 1; i <= dlugosc1; i++)
+            {
+                for (int j = 1; j <= dlugosc2; j++)
+                {
+                    if (sequence1[i - 1] == sequence2[j - 1])
+                    {
+                        dot[i, j] = dot[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        dot[i, j] = Math.Max(dot[i - 1, j], dot[i, j - 1]);
+                    }
+                }
+            }
+
+            int index = dot[dlugosc1, dlugosc2];
+            char[] nwp = new char[index];
+            int x = dlugosc1, y = dlugosc2;
+
+
+            while (x > 0 && y > 0)
+            {
+                if (sequence1[x - 1] == sequence2[y - 1])
+                {
+                    nwp[--index] = sequence1[x - 1];
+                    x--;
+                    y--;
+                }
+                else if (dot[x - 1, y] > dot[x, y - 1])
+                {
+                    x--;
+                }
+                else
+                {
+                    y--;
+                }
+            }
+            return new string(nwp);
+        }
+    }
+}
